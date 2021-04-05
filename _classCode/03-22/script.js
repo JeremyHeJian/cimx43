2156f345bbdfa7c0bf04ffd22c20f67c0be98fcd
JeremyH <kaonimaoba@gmail.com>
2021-04-05 13:12:12 -0400

    fix js link

--- _classCode/03-22/script.js
+++ _classCode/03-22/script.js
@@ -1,1 +1,18 @@
 console.log('WIP');
+
+let newLink = document.createElement('a');
+newLink.setAttribute('href', '#');
+newLink.setAttribute('class', 'readMore');
+newLink.innerHTML = 'Read more';
+newLink.style.display = 'inline-block';
+newLink.style.color = 'red';
+newLink.style.fontSize = '30px';
+
+let allParagraphs = document.getElementsByTagName('p');
+const firstParagraph = allParagraphs[0];
+
+firstParagraph.appendChild(newLink);
+
+for (let i = 0; i < allParagraphs.length; i++) {
+  
+}
